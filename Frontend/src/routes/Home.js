63134e26c0f19d6d8eaebc9ae11cf17d1abee51f
Tri Nguyen <nguyendinhtri@gmail.com>
2023-03-21 11:36:49 +0700

    Header-layout

--- Frontend/src/routes/Home.js
+++ Frontend/src/routes/Home.js
@@ -6,13 +6,11 @@ class Home extends Component {
 
     render() {
         const { isLoggedIn } = this.props;
-        let linkToRedirect = isLoggedIn ? '/system/user-manage' : '/login';
-
+        let linkToRedirect = isLoggedIn ? '/system/user-manage' : '/home';
         return (
             <Redirect to={linkToRedirect} />
         );
     }
-
 }
 
 const mapStateToProps = state => {
